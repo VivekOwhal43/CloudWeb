f199f1b14787e59ae8d62383af22e16162d390b6
VivekOwhal43 <69582519+VivekOwhal43@users.noreply.github.com>
2021-05-24 20:26:05 +0530

    First Commit

---
+++ cloudweb/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+def index_view(request):
+    return render(request,'index.html')
+
+def contact_view(request):
+    return render(request,'contact_us.html')
+
+def about_view(request):
+    return render(request,'about.html')
+
+'''def result_view(request):
+    user_input =  request.GET['user_input']
+    user_input= user_input.upper()
+    user_input += "This string is added"
+    return render(request,'result.html', {'user_home': user_input})
+'''
---
+++ cloudwebapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CloudwebappConfig(AppConfig):
+    name = 'cloudwebapp'

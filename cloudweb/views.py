3761ff6e47dc1f2d3147e5ca7e5d03ee361afcab
VivekOwhal43 <69582519+VivekOwhal43@users.noreply.github.com>
2021-06-04 14:08:05 +0530

    Update Version 0.1.11

--- cloudweb/views.py
+++ cloudweb/views.py
@@ -9,6 +9,9 @@ def contact_view(request):
 def about_view(request):
     return render(request,'about.html')
 
+def bedview(request):
+    return render(request,'covidbed.html')
+
 '''def result_view(request):
     user_input =  request.GET['user_input']
     user_input= user_input.upper()
